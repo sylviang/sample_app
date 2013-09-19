dcc5b02689ad2332250a1672e64a2fe82d98585e
Sylvia Ng <n.sylvia@gmail.com>
2013-09-19 14:14:47 +0800

    Initial commit

---
+++ vendor/bundle/gems/ffi-1.9.0/ext/ffi_c/siteconf20130918-86752-1m6w1vl.rb
@@ -0,0 +1,6 @@
+require 'rbconfig'
+dest_path = "./.gem.20130918-86752-8ubawi"
+RbConfig::MAKEFILE_CONFIG['sitearchdir'] = dest_path
+RbConfig::CONFIG['sitearchdir'] = dest_path
+RbConfig::MAKEFILE_CONFIG['sitelibdir'] = dest_path
+RbConfig::CONFIG['sitelibdir'] = dest_path
